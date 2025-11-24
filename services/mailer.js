f46b168309bcb2f2150b2ab9be0bcba9adc43b36
AkashKMathew <akashkmathew156@gmail.com>
2025-11-24 08:24:52 +0530

    smtp fix

--- services/mailer.js
+++ services/mailer.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 const transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth:{
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
