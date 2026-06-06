7fc80d1a7b49cb4208c95cd4313ca3d0d84acb58
AkashKMathew <akashkmathew156@gmail.com>
2026-06-06 20:11:15 +0530

    Revert "smtp fix"
    This reverts commit f46b168309bcb2f2150b2ab9be0bcba9adc43b36.

--- services/mailer.js
+++ services/mailer.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 const transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth:{
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
